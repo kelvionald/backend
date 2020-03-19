b30372cb2499a350446015861b370d3f8d225c55
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-19 22:27:23 +0300

    задание 2.2

---
+++ lab2/2.PrintArgs/2.PrintArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.PrintArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine("Number of arguments: {0}", args.Length);
+                Console.Write("Arguments: {0}", String.Join(" ", args));
+            }
+        }
+    }
+}
