4152db0cbf0d1193ebfc77140870e00d510ed8a1
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-19 22:40:04 +0300

    2.3 задание

---
+++ lab2/3.RemoveDuplicates/3.RemoveDuplicates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage 3.RemoveDuplicates.exe <input string>");
+                return;
+            }
+            Dictionary<char, bool> exitingChars = new Dictionary<char, bool>();
+            string line = args[0];
+            foreach (char ch in line)
+            {
+                if (!exitingChars.ContainsKey(ch))
+                {
+                    exitingChars[ch] = true;
+                    Console.Write(ch);
+                }
+            }
+        }
+    }
+}
