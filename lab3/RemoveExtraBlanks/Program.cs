5dd16e42452272d29be60fe802dfd94fa1fd61f5
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-27 21:45:50 +0300

    3.2 программа

--- lab3/RemoveExtraBlanks/Program.cs
+++ lab3/RemoveExtraBlanks/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RemoveExtraBlanksLibrary;
 
 namespace RemoveExtraBlanks
 {
@@ -10,6 +12,25 @@ namespace RemoveExtraBlanks
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage RemoveExtraBlanks.exe <inputFile> <outputFile>");
+                return;
+            }
+            string inputFile = args[0];
+            string outputFile = args[1];
+            StringProcessor stringProcessor = new StringProcessor();
+            using (StreamReader reader = new StreamReader(inputFile))
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    line = stringProcessor.RemoveExtraBlanks(line);
+                    writer.WriteLine(line);
+                }
+            }
         }
     }
 }
