16351858b19314561f8e75e3c3ab604983838a3f
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-27 21:31:56 +0300

    3.2 алгоритм и тесты

---
+++ lab3/RemoveExtraBlanksLibrary/StringProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveExtraBlanksLibrary
+{
+    enum State
+    {
+        SpaceTab,
+        Other
+    };
+
+    public class StringProcessor
+    {
+        private bool isWhitespace(char ch)
+        {
+            return ch == '\t' || ch == ' ';
+        }
+
+        public string RemoveExtraBlanks(string input)
+        {
+            string cleared = input.Trim(new char[] { ' ', '\t' });
+            string line = "";
+            State state = State.Other;
+            for (int i = 0; i < cleared.Length; i++)
+            {
+                char ch = cleared[i];
+                Console.WriteLine(ch);
+                switch (state)
+                {
+                    case State.Other:
+                        line += ch;
+                        if (isWhitespace(ch))
+                        {
+                            state = State.SpaceTab;
+                        }
+                        break;
+                    case State.SpaceTab:
+                        if (!isWhitespace(ch))
+                        {
+                            line += ch;
+                            state = State.Other;
+                        }
+                        break;
+                }
+            }
+            return line;
+        }
+    }
+}
---
+++ lab3/RemoveExtraBlanksTests/StringProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveExtraBlanksLibrary;
+
+namespace RemoveExtraBlanksTests
+{
+    [TestClass]
+    public class StringProcessorTests
+    {
+        [TestMethod]
+        public void TestRemoveExtraBlanks()
+        {
+            StringProcessor stringProcessor = new StringProcessor();
+            Assert.AreEqual("", stringProcessor.RemoveExtraBlanks("  "));
+            Assert.AreEqual("abcd", stringProcessor.RemoveExtraBlanks(" abcd\t "));
+            Assert.AreEqual("абвгд", stringProcessor.RemoveExtraBlanks(" абвгд \t"));
+            Assert.AreEqual("абв гд", stringProcessor.RemoveExtraBlanks("абв гд"));
+            Assert.AreEqual("абв гд", stringProcessor.RemoveExtraBlanks("абв  гд"));
+            Assert.AreEqual("абв\tcd", stringProcessor.RemoveExtraBlanks("абв\t  cd "));
+        }
+    }
+}
